064cd530dc638fd0fc2dad3f114ecf8809da1dc3
feldi <peter.feldtmann@gmail.com>
2015-10-28 19:20:36 +0100

    Minor tweaks

--- src/clj/swipl7_emil.clj
+++ src/clj/swipl7_emil.clj
@@ -12,18 +12,19 @@
 (defn emil2
   []
   (pl/consult "D:/ws/prolog/emil/emilTests.pl")
-  (let [query     (pl/new-q "test2(Varianten).")
+  (let [query     (pl/new-q "test2(Variante).")
         solutions (pl/run-q query)]
-    (println "emil " (pl/pl-to-text query)  " ==> " 
-             (map pl/pl-to-text solutions))))
+    (println "emil " (pl/pl-to-text query)  " ==> " )
+              (pl/show-solutions solutions)))
 
 (defn emil3
   []
   (pl/consult "D:/ws/prolog/emil/emilTests.pl")
-  (let [query     (pl/new-q "test3(Varianten).")
+  (let [query     (pl/new-q "test3(Variante).")
         solutions (pl/run-q query)]
-    (println "emil " (pl/pl-to-text query)  " ==> " 
-             (map pl/pl-to-text solutions))))
+    (println "emil " (pl/pl-to-text query)  " ==> " )
+              (pl/show-solutions solutions)
+              ))
   
 ;;------------------------------------------------------------------------
 ;; run emil
